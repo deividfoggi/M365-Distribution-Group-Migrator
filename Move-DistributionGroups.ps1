a972b8ed1cf2f3ac40f90cf0c1f765a6641556c7
deividfoggi <39340082+deividfoggi@users.noreply.github.com>
2019-09-13 14:48:08 -0300

    Move all $null statements from right to the left on all IFs to avoid problems when comparing arrays.

--- Move-DistributionGroups.ps1
+++ Move-DistributionGroups.ps1
@@ -1,4 +1,5 @@
-﻿#    Move-DistributionGroups.ps1
+﻿#
+#    Move-DistributionGroups.ps1
 #
 #    This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.  
 #    THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,        
@@ -7,7 +8,7 @@
 #    the object code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks
 #    to market Your software product in which the Sample Code is embedded; (ii) to include a valid copyright notice on
 #    Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us
-#    and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or resultfrom the 
+#    and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the 
 #    use or distribution of the Sample Code.
 #    Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained 
 #    within the Premier Customer Services Description.
@@ -329,9 +330,9 @@ Function ExportGroups{
 
         $arr = Get-DistributionGroup $obj.Name | Get-ADPermission | Where-Object{($_.ExtendedRights -like “*Send-As*”)}
         
-        If($arr -ne $null){
+        If($null -eq $arr){
             foreach($user In $arr){
-                    If($user -ne $null){
+                    If($null -ne $user){
                         $a = $user.User.ToString()
                         $b = $a.substring(0,2)
                         If($b -ne 'S-'){
@@ -615,11 +616,11 @@ Function CreateGroups{
                         }
                     }
                 }
-    
-                If($dl.RejectMessagesFrom -ne $null){
+
+                If($null -ne $dl.RejectMessagesFrom){
                     $arr = $dl.RejectMessagesFrom.Split(";")
                     foreach($i in $arr){
-                        If($i -ne $null){
+                        If($null -ne $i){
                             try{
                                 $acc3 = Get-EXODistributionGroup $dl.Name -ErrorAction SilentlyContinue
                                 $acc3.RejectMessagesFrom.Add($i)
